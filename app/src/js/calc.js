382eb205f43440586e5c7473250a53dee1401414
Adam Przybylski <adamprzybylski88@gmail.com>
2018-03-06 16:23:54 +0100

    start

---
+++ app/src/js/calc.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PointTarget from 'react-point'
+
+class CalcDisplay extends React.Component {
+	constructor() {
+  	  super();
+    }
+
+  	render() {
+
+  	}
+}
+
+class Calculator extends React.Component {
+
+}
+
+
+const app = document.getElementById('app')
+ReactDOM.render(
+	<Calculator/>,app
+)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+// const extractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	context: __dirname + '/app',
+	devtool: debug
+		? "inline-sourcemap"
+		: null,
+	entry: './src/js/calc.js',
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query: {
+					presets: [
+						'react', 'es2015', 'stage-0'
+					],
+					// plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-leacy']
+					plugins: ['react-html-attrs', 'transform-class-properties']
+				}
+			}
+		]
+	},
+	output: {
+		path: __dirname + '/app',
+		filename: './dist/app.js'
+	},
+	plugins: debug
+		? []
+		: [
+			new webpack.optimize.DedupePlugin(),
+			new webpack.optimize.OccurenceOrderPlugin(),
+			new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
+		],
+	node: {
+		fs: "empty"
+	}
+};
