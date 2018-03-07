e5c8ad17774c9d1d8a4c96a22afc63a5e02c4f6a
Adam Przybylski <adamprzybylski88@gmail.com>
2018-03-07 08:58:03 +0100

    displayed

--- app/src/js/calc.js
+++ app/src/js/calc.js
@@ -4,17 +4,73 @@ import PointTarget from 'react-point'
 
 // define module - calculator display
 class CalcDisplay extends React.Component {
-	constructor() {
-  	  super();
-    }
-
   	render() {
+		const { value } = this.props
 
+		return (
+			<div className="calculator-display">
+				<span>{value}</span>
+			</div>
+		)
   	}
 }
 
+class CalcKey extends React.Component {
+	render() {
+		const { ...props } = this.props
+
+		return (
+			<PointTarget>
+				<button { ...props }/>
+			</PointTarget>
+		)
+	}
+}
 
 class Calculator extends React.Component {
+	constructor() {
+	  super();
+	}
+
+	state = {
+    	displayVal: '0'
+  	};
+
+	render() {
+		const { displayVal } = this.state
+
+		return (
+			<div className="calculator">
+
+		        <CalcDisplay value={displayVal}/>
+
+		        <div className="calculator-keypad">
+					<div className="keys_inputs">
+						<CalcKey>7</CalcKey>
+						<CalcKey>8</CalcKey>
+						<CalcKey>9</CalcKey>
+
+						<CalcKey>4</CalcKey>
+						<CalcKey>5</CalcKey>
+						<CalcKey>6</CalcKey>
+
+						<CalcKey>1</CalcKey>
+						<CalcKey>2</CalcKey>
+						<CalcKey>3</CalcKey>
+
+						<CalcKey className="key-clear">AC</CalcKey>
+						<CalcKey>0</CalcKey>
+						<CalcKey className="key-dot">.</CalcKey>
+					</div>
+					<div className="keys_operations">
+						<CalcKey className="key-subtract">−</CalcKey>
+						<CalcKey className="key-add">+</CalcKey>
+						<CalcKey className="key-equals">=</CalcKey>
+					</div>
+				</div>
+			</div>
+		)
+	}
 
 }
 
