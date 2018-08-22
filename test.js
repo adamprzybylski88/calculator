2ce1115dc45cb6b1ddb60b1ca266fdafb2a3d208
Adam Przybylski <adamprzybylski88@gmail.com>
2018-08-22 08:24:50 +0200

    test

---
+++ test.js
@@ -0,0 +1,19 @@
+// const fs 							= require('fs')
+// const childProcess 					= require('child_process')
+// const spawn 						= childProcess.spawn
+
+// const displayInfo 					= require('./node_modules_lib/display-info.js')
+
+
+// let ls = spawn('jest', ['--colors']);
+
+// ls.stderr.on('data', (data) => {
+//     console.log(String(data));
+// });
+
+// ls.on('exit', (code) => {
+//     displayInfo('TESTS', 'finished', {
+//         color: 'green',
+//         status: 'start'
+//     })	
+// });
